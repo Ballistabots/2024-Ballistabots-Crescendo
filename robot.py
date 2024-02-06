d36980dff5adcdc0d6ce75514c839b4994fd944e
turtle4831 <jakethrun@gmail.com>
2024-02-06 16:51:38 -0600

    swerve field centric done. finally.

--- robot.py
+++ robot.py
@@ -1,77 +1,97 @@
+import math
+
 import wpilib
 import wpilib.drive
-import wpimath
-import wpimath.controller
-import wpimath.filter
+import wpilib.drive
+from wpimath.geometry import Rotation2d
+from wpimath.kinematics import ChassisSpeeds
 
-from components import drivetrain
+from robotcontainer import RobotContainer
 
 
 class MyRobot(wpilib.TimedRobot):
-   def robotInit(self) -> None:
-      """Robot initialization function"""
-      #self.controller1 = wpilib.XboxController(0)
-      self.controller1 = wpilib.Joystick(0)
-      # self.cotroller1 = wpilib.PS5Controller(0)
-      self.swerve = drivetrain.Drivetrain()
 
-      # Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0 to 1.
-      self.xspeedLimiter = wpimath.filter.SlewRateLimiter(3)
-      self.yspeedLimiter = wpimath.filter.SlewRateLimiter(3)
-      self.rotLimiter = wpimath.filter.SlewRateLimiter(3)
+   def robotInit(self):
+      """
+      This function is called upon program startup and
+      should be used for any initialization code.
+      """
+      self.robotContainer = RobotContainer()
+      self.drivetrain = self.robotContainer.drivetrain
 
 
-def autnomousInit(self) -> None:
-   pass
 
-def autonomousPeriodic(self) -> None:
-   self.driveWithJoystick(False)
-   self.swerve.updateOdometry()
+   def autonomousInit(self):
+      """This function is run once each time the robot enters autonomous mode."""
+      pass
 
-def TeleopInit(self) -> None:
-   print("Tele-op enabled")
-def TeleopPeriodic(self) -> None:
-   self.driveWithJoystick(True)
+   def autonomousPeriodic(self):
+      pass
+      """This function is called periodically during autonomous."""
+      """pathfindingCommand = AutoBuilder.pathfindToPose(
+      targetPose,
+      constraints,
+      goal_end_vel=0.0, # Goal end velocity in meters/sec
+      rotation_delay_distance=0.0 # Rotation delay distance in meters. This is how far the robot should travel before attempting to rotate.
+      )"""
 
-def disabledPeriodic(self) -> None:
-   pass
+   def teleopInit(self):
+      """This function is called once each time the robot enters teleoperated mode."""
+
+      #self.drivetrain.gyro.set_yaw(0)
+
+      """self.backLeftRotation.set(-self.BleftPID.calculate(self.BleftEnc.get_absolute_position()._value, 5.0))
+      self.frontLeftRotation.set(-self.FleftPID.calculate(self.FleftEnc.get_absolute_position()._value, 5.0))
+      self.backRightRotation.set(-self.BrightPID.calculate(self.BrightEnc.get_absolute_position()._value, 5.0))
+      self.frontRightRotation.set(-self.FrightPID.calculate(self.FrightEnc.get_absolute_position()._value, 5.0))"""
+
+   def teleopPeriodic(self):
+      """This function is called periodically during teleoperated mode."""
 
 
-def driveWithJoystick(self, fieldRelative: bool) -> None:
-   # Get the x speed. We are inverting this because Xbox controllers return
-   # negative values when we push forward.
-   xSpeed = (
-           -self.xspeedLimiter.calculate(
-              wpimath.applyDeadband(self.controller1.getLeftY(), 0.02)
 
-           )
-           * drivetrain.kMaxSpeed
-   )
+      self.joystick = wpilib.Joystick(0)
 
-   # Get the y speed or sideways/strafe speed we are inverting this because
-   # we want a positive value when we pull to the left. Xbox controllers
-   # return positive values when you pull to the right by default.
-   ySpeed = (
-           -self.yspeedLimiter.calculate(
-              wpimath.applyDeadband(self.controller1.getLeftX(), 0.02)
+      xspeed = self.joystick.getX()
+      yspeed = self.joystick.getY()
+      tspeed = self.joystick.getTwist()
 
-           )
-           * drivetrain.kMaxSpeed
-   )
+      yaw = -self.drivetrain.gyro.getYaw()
 
-   # Get the rate of angular rotation. We are inverting this because we want a
-   # positive value when we pull to the left (remember, CCW is positive in
-   # mathematics). Xbox controllers return positive values when you pull to
-   # the right by default.
-   rot = (
-           -self.rotLimiter.calculate(
-              wpimath.applyDeadband(self.controller1.getRightX(), 0.02)
+      h = yaw % 360
+      if h < 0:
+         h += 360
 
-           )
-           * drivetrain.kMaxSpeed
-   )
+      h2 = h / 360
 
-   self.swerve.drive(xSpeed, ySpeed, rot, fieldRelative, self.getPeriod())
+      heading = h2 * (math.pi * 2)
+
+      if abs(xspeed) < .15:
+         xspeed = 0
+      if abs(yspeed) < .15:
+         yspeed = 0
+      if abs(tspeed) < .15:
+         tspeed = 0
+
+      if xspeed == 0 and yspeed == 0 and tspeed == 0:  # xspeed == 0 and yspeed == 0 and tspeed == 0:
+         self.drivetrain.frontLeftDrive.set(0)
+         self.drivetrain.backRightDrive.set(0)
+         self.drivetrain.backLeftDrive.set(0)
+         self.drivetrain.frontRightDrive.set(0)
+
+         self.drivetrain.backLeftRotation.set(0)
+         self.drivetrain.backRightRotation.set(0)
+         self.drivetrain.frontLeftRotation.set(0)
+         self.drivetrain.frontRightRotation.set(0)
+      else:
+         #field centric
+         #speeds = ChassisSpeeds.fromFieldRelativeSpeeds(-yspeed * 0.5, xspeed * 0.5, tspeed * 0.5, Rotation2d(self.drivetrain.gyro.getRoll()))
+         #robot centric
+         speeds = ChassisSpeeds.fromRobotRelativeSpeeds(-yspeed * 0.2, xspeed * 0.2, tspeed * 0.2, Rotation2d(heading))
+         print(heading)
+         self.drivetrain.driveFromChassisSpeeds(speeds)
+
+
 
 if __name__ == "__main__":
    wpilib.run(MyRobot)
---
+++ robotcontainer.py
@@ -0,0 +1,9 @@
+from drivetrain import DriveTrain
+
+
+class RobotContainer:
+   def __init__(self) -> None:
+      self.drivetrain = DriveTrain()
+
+   def getAutoCommand(self):
+      return self.drivetrain.getAutonomousCommand()
