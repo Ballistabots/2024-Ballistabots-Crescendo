308a5ae58b4458b50fe178311f9574ccc2fcda57
turtle4831 <131495937+turtle4831@users.noreply.github.com>
2024-02-13 20:09:07 -0600

    Add files via upload

---
+++ 2024-Ballistabots-Crescendo/components/physics.py
@@ -0,0 +1,11 @@
+class Physics:
+   flyWheelMaxRPM = 6380
+   flyWheelGearRatio = 1 / 1
+   maxShootingAngle = 0  # change this once u get it
+   minShootingAngle = 0  # change this once u get it
+
+   def __init__(self):
+      pass
+
+   def calcAngle(self):
+      pass
